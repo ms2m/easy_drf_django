98890a24e320f4e6d61306e812a6a88ae7879419
Manmohan Singh <manmohansingh@Manmohans-MacBook-Pro-2.local>
2021-09-20 01:48:19 +0530

    added generic view in views.py

--- checklist/core/permissions.py
+++ checklist/core/permissions.py
@@ -6,5 +6,7 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # print('aaaaaa..   ... {} ... {}'.format(obj, dir(obj)))
         # print('object user: {} \t requested user... {}\n\n'.format(obj.user, request.user))
-        if request.method in permissions.SAFE_METHODS:
-            return obj.user == request.user
+        # print(request.method , permissions.SAFE_METHODS)
+        # if request.method in permissions.SAFE_METHODS:
+            # pass
+        return obj.user == request.user
--- checklist/core/views.py
+++ checklist/core/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.http import Http404
 
 from rest_framework import serializers
@@ -5,6 +6,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    CreateAPIView
+
+)
 
 from core.models import CheckList, CheckListItem
 from core.permissions import IsOwner
@@ -16,111 +23,139 @@ from core.serializers import CheckListSeralizer, CheckListItemSeralizer
 ## serialization: converts python data types to Json
 ## deserialization: converts Json to python data
 
-class CheckListsAPIView(APIView):
-    seriailizer_class = CheckListSeralizer
+class CheckListsAPIView(ListCreateAPIView):
+    """
+    Listing, Creation
+    """
+    ## converting into genric view.
+    serializer_class = CheckListSeralizer
     permission_classes = [IsAuthenticated, IsOwner]
 
-    def get(self, request, format=None):
-        # data = CheckList.objects.all()
-        ## access only which user created data.
-        data = CheckList.objects.filter(user=request.user)
-        serializer = self.seriailizer_class(data, many=True)
-        serilalized_data = serializer.data
-        return Response(serilalized_data, status=status.HTTP_200_OK)
+    def get_queryset(self):
+        queryset = CheckList.objects.filter(user=self.request.user)
+        return queryset
 
-    def post(self, request, format=None):
-        # context is for resolution of below error i.e. for accessing user data login
-        #  return serializer_field.context['request'].user
-        serializer = self.seriailizer_class(data=request.data, context={"request": request})
-        if serializer.is_valid():
-            serializer.save()
-            serialized_data = serializer.data
-            return Response(serialized_data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def get(self, request, format=None):
+    #     # data = CheckList.objects.all()
+    #     ## access only which user created data.
+    #     data = CheckList.objects.filter(user=request.user)
+    #     serializer = self.serializer_class(data, many=True)
+    #     serilalized_data = serializer.data
+    #     return Response(serilalized_data, status=status.HTTP_200_OK)
+
+    # def post(self, request, format=None):
+    #     # context is for resolution of below error i.e. for accessing user data login
+    #     #  return serializer_field.context['request'].user
+    #     serializer = self.serializer_class(data=request.data, context={"request": request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         serialized_data = serializer.data
+    #         return Response(serialized_data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CheckListAPIView(APIView):
-    seriailizer_class = CheckListSeralizer
+class CheckListAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    Retrivel, Update, Destroy
+    """
+    ## converting into genric view.
+    serializer_class = CheckListSeralizer
     permission_classes = [IsAuthenticated, IsOwner]
 
-    def get_obj(self, pk):
-        try:
-            obj = CheckList.objects.get(pk=pk)
-            self.check_object_permissions(self.request, obj)
-            return obj
-        except Exception as e:
-            if 'permission' in str(e):
-                raise e
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        serializer = self.seriailizer_class(self.get_obj(pk))
-        serialized_data = serializer.data
-        return Response(serialized_data, status=status.HTTP_200_OK)
-
-    def put(self, request, pk, format=None):
-        checklist = self.get_obj(pk)
-        serializer = self.seriailizer_class(checklist, data=request.data, context={"request": request})
-        if serializer.is_valid():
-            serializer.save()
-            serialized_data = serializer.data
-            return Response(serialized_data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        checklist = self.get_obj(pk)
-        checklist.delete()
-        # serializer = self.seriailizer_class()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_queryset(self):
+        queryset = CheckList.objects.filter(user=self.request.user)
+        return queryset
 
 
-class CheckListItemCreateAPIView(APIView):
-    seriailizer_class = CheckListItemSeralizer
+    # def get_obj(self, pk):
+    #     try:
+    #         obj = CheckList.objects.get(pk=pk)
+    #         self.check_object_permissions(self.request, obj)
+    #         return obj
+    #     except Exception as e:
+    #         if 'permission' in str(e):
+    #             raise e
+    #         raise Http404
+
+    # def get(self, request, pk, format=None):
+    #     serializer = self.serializer_class(self.get_obj(pk))
+    #     serialized_data = serializer.data
+    #     return Response(serialized_data, status=status.HTTP_200_OK)
+
+    # def put(self, request, pk, format=None):
+    #     checklist = self.get_obj(pk)
+    #     serializer = self.serializer_class(checklist, data=request.data, context={"request": request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         serialized_data = serializer.data
+    #         return Response(serialized_data, status=status.HTTP_200_OK)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def delete(self, request, pk, format=None):
+    #     checklist = self.get_obj(pk)
+    #     checklist.delete()
+    #     # serializer = self.serializer_class()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CheckListItemCreateAPIView(CreateAPIView):
+    """
+    Creation
+    """
+    serializer_class = CheckListItemSeralizer
     permission_classes = [IsAuthenticated, IsOwner]
 
-    # Code for creation
-    def post(self, request, format=None):
-        serializer = self.seriailizer_class(data=request.data, context={"request": request})
-        if serializer.is_valid():
-            serializer.save()
-            serialized_data = serializer.data
-            return Response(serialized_data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # # Code for creation
+    # def post(self, request, format=None):
+    #     serializer = self.serializer_class(data=request.data, context={"request": request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         serialized_data = serializer.data
+    #         return Response(serialized_data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CheckListItemAPIView(APIView):
-    seriailizer_class = CheckListItemSeralizer
+class CheckListItemAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    Retrivel, Update, Destroy
+    """
+    serializer_class = CheckListItemSeralizer
     permission_classes = [IsAuthenticated, IsOwner]
 
-    def get_obj(self, pk):
-        try:
-            obj = CheckListItem.objects.get(pk=pk)
-            self.check_object_permissions(self.request, obj)
-            return obj
-            # return CheckListItem.objects.get(pk=pk)
-        except Exception as e:
-            if 'permission' in str(e):
-                raise e
-            raise Http404
+    def get_queryset(self):
+        queryset = CheckListItem.objects.filter(user=self.request.user)
+        return queryset
 
-    def get(self, request, pk, format=None):
-        checklist_item = self.get_obj(pk)
-        serializer = self.seriailizer_class(checklist_item)
-        serialized_data = serializer.data
-        return Response(serialized_data, status=status.HTTP_200_OK)
 
-    def put(self, request, pk, format=None):
-        checklist_item = self.get_obj(pk)
-        serializer = self.seriailizer_class(checklist_item, data=request.data, context={"request": request})
-        if serializer.is_valid():
-            serializer.save()
-            serialized_data = serializer.data
-            return Response(serialized_data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def get_obj(self, pk):
+    #     try:
+    #         obj = CheckListItem.objects.get(pk=pk)
+    #         self.check_object_permissions(self.request, obj)
+    #         return obj
+    #         # return CheckListItem.objects.get(pk=pk)
+    #     except Exception as e:
+    #         if 'permission' in str(e):
+    #             raise e
+    #         raise Http404
 
-    def delete(self, request, pk, format=None):
-        checklist_item = self.get_obj(pk)
-        checklist_item.delete()
-        # serializer = self.seriailizer_class()
-        return Response(status=status.HTTP_204_NO_CONTENT) 
+    # def get(self, request, pk, format=None):
+    #     checklist_item = self.get_obj(pk)
+    #     serializer = self.serializer_class(checklist_item)
+    #     serialized_data = serializer.data
+    #     return Response(serialized_data, status=status.HTTP_200_OK)
+
+    # def put(self, request, pk, format=None):
+    #     checklist_item = self.get_obj(pk)
+    #     serializer = self.serializer_class(checklist_item, data=request.data, context={"request": request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         serialized_data = serializer.data
+    #         return Response(serialized_data, status=status.HTTP_200_OK)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def delete(self, request, pk, format=None):
+    #     checklist_item = self.get_obj(pk)
+    #     checklist_item.delete()
+    #     # serializer = self.serializer_class()
+    #     return Response(status=status.HTTP_204_NO_CONTENT) 
 
